060d80d7a98f677939900a34eadb21ae23a535c2
Rais Zainuri <raiszainuri09@gmail.com>
2021-06-20 20:38:52 +0700

    change

--- controller/user.php
+++ controller/user.php
@@ -2,6 +2,24 @@
 
 require('koneksi.php');
 
-function registerUser(){
-    
+$clmn = [''];
+getListWarga();
+
+function getListWarga(){
+  global $link;
+  
+  $sql    = "SELECT * FROM user WHERE user_type = 2";
+  $result = $link->query($sql);
+  echo "test";
+  $rows   = $result->num_rows;
+  $reply  = "";
+
+  if ($rows > 0) {
+    $nomor = 1;
+    while($row = $result->fetch_assoc()){
+      echo $row;
+    }
+  }
+
+  echo "test";
 }
